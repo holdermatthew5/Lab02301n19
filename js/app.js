4f8666b4b7022de2e60e55426780d2b7300179e3
Michael Campbell <cam3000@gmail.com>
2020-10-01 12:50:30 -0700

    added header and option

--- js/app.js
+++ js/app.js
@@ -1,6 +1,18 @@
 'use strict'
 
-function Horn (object {
-  this.imgUrl = object.image_url
+const hornArrary = [];
+
+$.ajax
+
+function Horn(object) {
+  this.imgUrl = object.image_url;
+  this.title = object.title;
+  this. description = object.description;
+  this.keyword = object.keyword;
+  this.horns = object.horns;
+  hornArrary.push(this);
+}
+
+Horn.prototype.render = function () {
   
-})
+}
