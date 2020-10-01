4ed76c3f001a3ac173525633a40fb16a4d0930f5
Michael Campbell <cam3000@gmail.com>
2020-10-01 14:00:51 -0700

    added link to jquery and added prototype render

--- js/app.js
+++ js/app.js
@@ -2,18 +2,29 @@
 
 const hornArrary = [];
 
-$.ajax
+$.ajax('data/page-1.json', { method: 'GET', dataType: 'JSON' })
+  .then(hornInfo => {
+    new Horn(horn).render();
+  })
 
 function Horn(object) {
   this.imgUrl = object.image_url;
   this.title = object.title;
-  this. description = object.description;
+  this.description = object.description;
   this.keyword = object.keyword;
   this.horns = object.horns;
   hornArrary.push(this);
 }
 
 Horn.prototype.render = function () {
+  const template = $('#photo-template').html();
+  const $newSection = $('<section id="${this.keyword}">${template}</section>');
+  $newSection.find('h2').text(this.title);
+  $newSection.find(p).text(this.title)
+  $newSection.find
+
+
+
 
 }
 
